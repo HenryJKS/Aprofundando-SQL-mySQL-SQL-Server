73a6e4f525e5884857a812ce3868930d2f524ac5
HenryJKS <99285971+HenryJKS@users.noreply.github.com>
2023-03-09 11:56:54 -0300

    Update DBA.sql

--- mySQL/DBA - MySQL/DBA.sql
+++ mySQL/DBA - MySQL/DBA.sql
@@ -8,3 +8,15 @@ mysqldump -uroot -p --databases nomedatabela > localdobackup
 
 /* Verificar o custo da consulta */
 EXPLAIN FORMAT=JSON (Consulta) \G
+
+/* Adicionar privilégios por linha de comando */
+grant select, insert on database.nomedatabela to 'usuario'@'conexao';
+
+/* Mostrar todos usuários e seus privilégios */
+select * from mysql.user where user = 'root';
+
+/* Mostrar o privilégios */
+show grants for 'root'@'localhost';
+
+/* Remover todos privilégios */
+revoke all privileges, grant option from 'user'@'localhost';
